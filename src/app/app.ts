b832c161dbee6bf1bb68884243020f12bf82e3ef
Ignacio Manuel <ignaciosolano.00105@gmail.com>
2026-01-11 14:41:53 -0600

    mejora visual del footer 11 de Enero del 26

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './shared/components/header/header'; 
-import { FooterComponent } from './shared/components/footer/footer';
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet,
-    HeaderComponent,
-    FooterComponent
+    RouterOutlet
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
